e500358a8a318ab412e93dd2ac492bb2017dc931
Xiaowei Zhou <41534161+Xiaowei-Zhou@users.noreply.github.com>
2018-08-13 22:10:52 +1000

    Data Version 180813

--- getInit.sh
+++ getInit.sh
@@ -7,10 +7,10 @@ echo 已下载国服init
 wget -O jp_init.json http://version.jp.warshipgirls.com/index/getInitConfigs
 echo 已下载日服init
 wget -O tw_init.json http://version.jr.moepoint.tw/index/getInitConfigs
-echo 已下载台服CBT服init
+echo 已下载台服init
 ./buildCsv.py cn
 echo 创建国服船只列表cn_全船表.csv成功
 ./buildCsv.py jp
 echo 创建日服船只列表jp_全船表.csv成功
 ./buildCsv.py tw
-echo 创建台服CBT服船只列表tw_全船表.csv成功
+echo 创建台服船只列表tw_全船表.csv成功
